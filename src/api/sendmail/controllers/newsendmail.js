351a0f927743b12c9f35502b262965c32bfe7f8e
rdominguez <rdominguez@soaint.com>
2023-01-11 17:51:51 -0500

    changing header type of the request

--- src/api/sendmail/controllers/newsendmail.js
+++ src/api/sendmail/controllers/newsendmail.js
@@ -1,12 +1,12 @@
 module.exports = {
   customAction: async (ctx) => {
-    //console.log(ctx.request);
 
-    // console.log(ctx.request.body);
-    //const postParams = JSON.parse(ctx.request.body);
 
-    // const postParamsStr = ctx.request.body;
-     const postParams = JSON.parse(ctx.request.body);
+    //request-header: --header 'Content-Type: application/json'
+    const postParams = ctx.request.body;
+
+    //request-header: --header 'Content-Type: text/plain' 
+    // const postParams = JSON.parse(ctx.request.body);
 
     strapi.log.debug(postParams);
 
@@ -14,16 +14,14 @@ module.exports = {
 
     const vfrom = postParams.from;
     const vsubject = postParams.subject;
-//TODO: limitar el tamano de los ficheor a enviar
+
+    //TODO: limitar el tamano de los ficheor a enviar
     const vhtml = postParams.html;
     if (postParams.attachments) {
-        var attachments = postParams.attachments;
-      //  strapi.log.debug("viene attachment");
-   // } else {
-     // strapi.log.debug("NO viene attachment");
+      var attachments = postParams.attachments;
     }
 
-   // const fichero ="e1xydGYxXGFuc2lcYW5zaWNwZzEyNTJcY29jb2FydGYyNjM2Clxjb2NvYXRleHRzY2FsaW5nMFxjb2NvYXBsYXRmb3JtMHtcZm9udHRibFxmMFxmc3dpc3NcZmNoYXJzZXQwIEhlbHZldGljYTt9CntcY29sb3J0Ymw7XHJlZDI1NVxncmVlbjI1NVxibHVlMjU1O30Ke1wqXGV4cGFuZGVkY29sb3J0Ymw7O30KXHBhcGVydzExOTAwXHBhcGVyaDE2ODQwXG1hcmdsMTQ0MFxtYXJncjE0NDBcdmlld3cxMTUyMFx2aWV3aDg0MDBcdmlld2tpbmQwClxwYXJkXHR4NTY2XHR4MTEzM1x0eDE3MDBcdHgyMjY3XHR4MjgzNFx0eDM0MDFcdHgzOTY4XHR4NDUzNVx0eDUxMDJcdHg1NjY5XHR4NjIzNlx0eDY4MDNccGFyZGlybmF0dXJhbFxwYXJ0aWdodGVuZmFjdG9yMAoKXGYwXGZzMjQgXGNmMCAwMTIzNDU2Nzg5YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXpcClxwYXJkXHR4NTY2XHR4MTEzM1x0eDE3MDBcdHgyMjY3XHR4MjgzNFx0eDM0MDFcdHgzOTY4XHR4NDUzNVx0eDUxMDJcdHg1NjY5XHR4NjIzNlx0eDY4MDNccGFyZGlybmF0dXJhbFxwYXJ0aWdodGVuZmFjdG9yMApcY2YwIDAxMjM0NTY3ODlhYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5elwKMDEyMzQ1Njc4OWFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6XAowMTIzNDU2Nzg5YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXp9";
+    // const fichero ="e1xydGYxXGFuc2lcYW5zaWNwZzEyNTJcY29jb2FydGYyNjM2Clxjb2NvYXRleHRzY2FsaW5nMFxjb2NvYXBsYXRmb3JtMHtcZm9udHRibFxmMFxmc3dpc3NcZmNoYXJzZXQwIEhlbHZldGljYTt9CntcY29sb3J0Ymw7XHJlZDI1NVxncmVlbjI1NVxibHVlMjU1O30Ke1wqXGV4cGFuZGVkY29sb3J0Ymw7O30KXHBhcGVydzExOTAwXHBhcGVyaDE2ODQwXG1hcmdsMTQ0MFxtYXJncjE0NDBcdmlld3cxMTUyMFx2aWV3aDg0MDBcdmlld2tpbmQwClxwYXJkXHR4NTY2XHR4MTEzM1x0eDE3MDBcdHgyMjY3XHR4MjgzNFx0eDM0MDFcdHgzOTY4XHR4NDUzNVx0eDUxMDJcdHg1NjY5XHR4NjIzNlx0eDY4MDNccGFyZGlybmF0dXJhbFxwYXJ0aWdodGVuZmFjdG9yMAoKXGYwXGZzMjQgXGNmMCAwMTIzNDU2Nzg5YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXpcClxwYXJkXHR4NTY2XHR4MTEzM1x0eDE3MDBcdHgyMjY3XHR4MjgzNFx0eDM0MDFcdHgzOTY4XHR4NDUzNVx0eDUxMDJcdHg1NjY5XHR4NjIzNlx0eDY4MDNccGFyZGlybmF0dXJhbFxwYXJ0aWdodGVuZmFjdG9yMApcY2YwIDAxMjM0NTY3ODlhYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5elwKMDEyMzQ1Njc4OWFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6XAowMTIzNDU2Nzg5YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXp9";
 
     try {
       let mensaje = {
@@ -31,10 +29,10 @@ module.exports = {
         from: vfrom,
         subject: vsubject,
         html: vhtml
-       
+
       };
 
-      if (postParams.attachments) { 
+      if (postParams.attachments) {
         mensaje.attachments = attachments;
       }
 
